f9ff030a7aa4b4eb61482d1b684372b337c85bf0
Thuan Tran <thuantran2704@gmail.com>
2024-02-04 16:16:36 -0500

    add index.js

--- bookingApp/src/pages/RegisterPage.jsx
+++ bookingApp/src/pages/RegisterPage.jsx
@@ -1,11 +1,16 @@
 import{Link} from "react-router-dom"
+import React, {useState} from 'react';
 export default function RegisterPage(){
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    
     return(
         <div className="mt-4 grow flex items-center justify-around">
             <div className = "mb-64">
             <h1 className="text-4xl text-center mb-4">Register</h1>
             <form className="max-w-md mx-auto">
-                <input type = "password" placeholder={'Joe Mama'}/>
+                <input type ="name" placeholder={'your name'}/>
                 <input type="email" placeholder={'your@email.com'}/>
                 <input type = "password" placeholder={'password'}/>
                 <button className = "primary">Register</button>
