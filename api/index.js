2a19c23eca210f82a195ea4c768c4293d264e12d
Thuan Tran <thuantran2704@gmail.com>
2024-03-10 20:21:57 -0400

    API files

--- api/index.js
+++ api/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors({
+    credentials: true,
+    origin: 'http://127.0.0.1:5173',
+
+}));
 
 app.get('/test', (req,res)=> {
     res.json('test good');
-})
+});
+
 
 
 
--- bookingApp/src/pages/RegisterPage.jsx
+++ bookingApp/src/pages/RegisterPage.jsx
@@ -5,14 +5,19 @@ export default function RegisterPage(){
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     
+    function registerUser(ev) {
+        ev.preventDefault;
+        axios.get('http://localhost:4000/test');
+
+    }
     return(
         <div className="mt-4 grow flex items-center justify-around">
             <div className = "mb-64">
             <h1 className="text-4xl text-center mb-4">Register</h1>
-            <form className="max-w-md mx-auto">
-                <input type ="name" placeholder={'your name'}/>
-                <input type="email" placeholder={'your@email.com'}/>
-                <input type = "password" placeholder={'password'}/>
+            <form className="max-w-md mx-auto" onSubmit = {registerUser}>
+                <input type ="name" placeholder={'your name'} value = {name} onChange = {ev => {setName(ev.target.value); console.log(ev);}}/>
+                <input type="email" placeholder={'your@email.com'} value = {email} onChange = {ev => setEmail(ev.target.value)}/>
+                <input type = "password" placeholder={'password'} value = {password} onChange={ev => setPassword(ev.target.value)}/>
                 <button className = "primary">Register</button>
                 
                 <div className = "text-center py-2 text-gray-500">
