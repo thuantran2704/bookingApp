d0c02ecbb923cdc21a83fb53007b1d3e0151446c
Thuan Tran <thuantran2704@gmail.com>
2024-03-14 03:48:50 -0400

    update front end and middleware for login page

--- api/index.js
+++ api/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require("mongoose");
+const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
+const User = require('./models/user.js');
+const bcrypt = require('bcryptjs');
+
+
 const app = express();
 
 dotenv.config();
@@ -9,6 +14,10 @@ dotenv.config();
 app.use(express.json())
 
 const origin = 'http://localhost:5173';
+
+const  salt = bcrypt.genSaltSync(10);
+
+const jwtSecret = 'JT1qnpNpJe';
 
 app.use(cors({
     credentials: true,
@@ -21,9 +30,37 @@ app.get('/test', (req,res)=> {
     res.json('test good');
 });
 
-app.post('/register', (req,res) => {
+app.post('/register', async (req,res) => {
     const {name, email, password } = req.body;
-    res.json({name,email,password});
+     try{
+    const userDocument = await User.create({
+        name,
+        email,
+        password:bcrypt.hashSync(password, salt),
+    });
+
+    res.json(userDocument);
+} catch(e){
+    res.status(422).json(e);
+}
+});
+
+app.post('/login', async (req,res) =>{
+    const {email, password} = req.body;
+    const userDocument = await User.findOne({email: email});
+    if(userDocument){
+        const accepted = bcrypt.compareSync(password, userDocument.password);
+        if(accepted){
+            jwt.sign({email: userDocument.email, id:userDocument._id}, jtwSecret, {}, (err, token) => {
+                if(err) throw err;
+                res.cookie('token', token).json('login successfully');
+            })
+            res.cookie('token', '').json('Went through');
+        }
+    } else {
+        alert('Password or Account incorrect');
+        res.status(422).json('Wrong Password');
+    }
 })
 
 
--- bookingApp/src/pages/RegisterPage.jsx
+++ bookingApp/src/pages/RegisterPage.jsx
@@ -6,22 +6,33 @@ export default function RegisterPage(){
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     
-    function registerUser(ev) {
+    async function registerUser(ev) {
         ev.preventDefault();
-        axios.post('/register',{
+    try{
+        await axios.post('/register',{
             name,
             email,
             password
         });
+        alert('Registration Successful.');
+    } catch(e){
+        alert("Regsistration Failed. Please try again later");
     }
+}
     return(
         <div className="mt-4 grow flex items-center justify-around">
             <div className = "mb-64">
             <h1 className="text-4xl text-center mb-4">Register</h1>
             <form className="max-w-md mx-auto" onSubmit = {registerUser}>
-                <input type ="name" placeholder={'your name'} value = {name} onChange = {ev => setName(ev.target.value)}/>
-                <input type="email" placeholder={'your@email.com'} value = {email} onChange = {ev => setEmail(ev.target.value)}/>
-                <input type = "password" placeholder={'password'} value = {password} onChange={ev => setPassword(ev.target.value)}/>
+                <input type ="name" placeholder='your name' value = {name} onChange = {ev => setName(ev.target.value)}/>
+                <input type="email"
+                        placeholder= "your@email.com" 
+                        value = {email} 
+                        onChange = {ev => setEmail(ev.target.value)}/>
+                <input type = "password" 
+                        placeholder="password"
+                        value = {password} 
+                        onChange={ev => setPassword(ev.target.value)}/>
                 <button className = "primary">Register</button>
                 
                 <div className = "text-center py-2 text-gray-500">
